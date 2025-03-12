603940a2c6dc6f1e54f654c931d51fb9fe02321b
HarshalM22 <harshalmankar61@gmail.com>
2025-03-12 12:43:42 +0530

    schema and types update

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,13 +1,14 @@
 import {z} from "Zod"
 
 export const CreateUSerSchema = z.object({
+    name : z.string(),
     password : z.string(),
     username : z.string(),
     email : z.string().min(3).max(50)
 })
 export const SigninSchema = z.object({
     email : z.string().min(3).max(50),
-    password : z.string(),
+    password : z.string()
 
 })
 export const CreateRoomSchema = z.object({
